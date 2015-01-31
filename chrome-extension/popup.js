461ed9d84880e397b624c180e5ad8be9aa8b83f0
n-zeplo <nzeplowitz@gmail.com>
2015-01-31 11:44:47 -0500

    Redo JavaScript With Pairing Object

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,19 +1,65 @@
 $(document).ready(function(){
-  var pairing_duration_ms;
+  var user1, user2, currentSession, startTime, endTime, currentInterval, totalTimeWorking, pairingDurationMs, timer;
+  var sessions = []
 
   $('button.duration-button').on('click', function(event){
     event.preventDefault();
 
     if($('input.duration').val() !== "" ){
-      pairing_duration_ms = parseInt( $('input.duration').val() ) * 6000;
+      currentInterval = parseInt($('input.duration').val());
+      pairingDurationMs = currentInterval * 60000;
     } else {
       alert("Please Enter A Number");
     }
   });
 
+  $('.pause_button').on('click', function(){
+
+  })
+
   $('button.start-pairing').on('click', function(){
-    setTimeout(function(){
-      alert('Time to Switch It ^');
-    }, pairing_duration_ms);
+    var date = new Date();
+    startTime = date.getTime();
+    currentSession = sessions.push(PairingSession(user1, user2));
+    startTimerCount();
   });
+
+  function PairingSession(){
+    this.drive = user1;
+    this.navigate = user2;
+    this.timeWorked;
+    this.timePaused;
+  }
+
+  function startTimerCount(){
+    timer = setInterval(function(){
+      totalTimeWorking ++;
+      currentInterval --;
+      updateTimerCountdown();
+      checkDuration();
+    }, 60000);
+  }
+
+  function calculatePauseTime(){
+    pause_time = endTime - startTime - totalTimeWorking;
+    currentSession.timePaused = pause_time;
+  }
+
+  function stopTimer(){
+    window.clearInterval(timer);
+  }
+
+  function checkDuration(){
+    if(currentInterval === 0){
+      currentInterval = pairingDurationMs / 60000;
+      alert('Switch it ^');
+    }
+  }
+
+  function updateTimerCountdown(){
+    $('.countdown').val(currentInterval);
+  }
 });
+
+
+
