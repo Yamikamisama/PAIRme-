d2e48bd3210ed5e0f61a6e7ce5091d37dd4e8dfe
Steph Reaves <stephanie.reaves@gmail.com>
2015-01-31 15:59:06 -0500

    refactor of algo

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,21 @@
 class Feedback < ActiveRecord::Base
   belongs_to :user
   belongs_to :session
+
+  def average(array)
+  	all_ratings = array 
+    all_ratings.reduce(:+) / all_ratings.length
+  end
+
+  def driver_score
+    rating1
+  end
+
+  def navigator_score
+    rating2
+  end
+
+  def overall_score
+    rating3
+  end
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,18 @@
 class Session < ActiveRecord::Base
   has_many :users
   has_many :feedbacks
+
+  
+  def add_feedback(feedback)
+    pair_id = [self.student1.id, self.student2.id]
+
+    if pair_id[0] == feedback.student.id
+      self.feedback1 = feedback
+      pair_id.delete(feedback.student.id)
+    end
+    if pair_id[1] == feedback.student.id
+      self.feedback2 = feedback
+      pair_id.delete(feedback.student.id)
+    end
+  end
 end
