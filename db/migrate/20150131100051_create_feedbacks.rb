d741d97736976bdfcbaff5e154e681e600cc737e
Steph Reaves <stephanie.reaves@gmail.com>
2015-01-31 10:15:12 -0500

    feedback migration

--- db/migrate/20150131100051_create_feedbacks.rb
+++ db/migrate/20150131100051_create_feedbacks.rb
@@ -1,4 +1,10 @@
 class CreateFeedbacks < ActiveRecord::Migration
   def change
+  	create_table :feedbacks do |t|
+  		t.integer :q1
+  		t.integer :q2
+  		t.integer :q3
+  		t.text 	  :text 
+  	end 
   end
 end
