0a769856c14d114c03f08c2d82dce411bd473916
Steph Reaves <stephanie.reaves@gmail.com>
2015-01-31 16:09:42 -0500

    finish make pair algo

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,20 +2,20 @@ class Feedback < ActiveRecord::Base
   belongs_to :user
   belongs_to :session
 
-  def average(array)
-  	all_ratings = array 
-    all_ratings.reduce(:+) / all_ratings.length
-  end
+  # def average(array)
+  # 	all_ratings = array 
+  #   all_ratings.reduce(:+) / all_ratings.length
+  # end
 
-  def driver_score
-    rating1
-  end
+  # def driver_score
+  #   rating1
+  # end
 
-  def navigator_score
-    rating2
-  end
+  # def navigator_score
+  #   rating2
+  # end
 
-  def overall_score
-    rating3
-  end
+  # def overall_score
+  #   rating3
+  # end
 end
