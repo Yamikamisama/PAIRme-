25ddd32bce8364121007c6bfc9d73f47a34b5b65
Steph Reaves <stephanie.reaves@gmail.com>
2015-01-31 11:42:54 -0500

    rebase commit

--- db/migrate/20141105111536_create_users.rb
+++ db/migrate/20141105111536_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :email
       t.string :password_digest
+      t.string :quirk 
       t.integer :driver
       t.integer :navigator
       t.references :organization
