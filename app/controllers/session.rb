a6924774b0c42dc392ab1f85bb03b774cfdfe94e
n-zeplo <nzeplowitz@gmail.com>
2015-01-31 17:55:02 -0500

    Get AJAX Working at ALL costs

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,24 +1,22 @@
-get '/session' do 
-
-	erb: :session
-end 
-
 post '/session' do
-	user1 = User.find_by(email: params[:user1][:email]).try(:authenticate, params[:user1]) 
-	user2 = User.find_by(email: params[:user2][:email]).try(:authenticate, params[:user2])  
-	
-	if user1 && user2 
+	user1 = User.find_by(email: params["user1"]["email"])
+	user2 = User.find_by(email: params["user2"]["email"])
+	p "*" * 40
+	p params["user2"]["email"]
+	p user1
+	p user2
+	if user1 && user2
 		Session.create(user1: user1, user2: user2)
-		json {user1: user1.email, user2: user2.email}
-	else 
-		500 
-	end 
+		{ :user1 => user1.email, :user2 => user2.email}.to_json
+	else
+		500
+	end
 
-end 
-
-post '/session/data' do 
-	session = Session.find_by(user1: 
-
-	cur_session = session.pop 
-	cur_session.update(:active , :downtime, :duration)
 end
+
+# post '/session/data' do
+# 	session = Session.find_by(user1:
+
+# 	cur_session = session.pop
+# 	cur_session.update(:active , :downtime, :duration)
+# end
