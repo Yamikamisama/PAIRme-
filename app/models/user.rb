6cc1b1d66fd33e1ac92a514db4bddd59beaf9664
ivanyukio <ivan.yk14@gmail.com>
2015-01-31 12:22:30 -0500

    fix merge conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,57 @@
 class User < ActiveRecord::Base
   has_secure_password
-  
+
   validates :email, :password, presence: true
 
   has_many :sessions
   has_many :feedbacks
   belongs_to :organization
+# class User < ActiveRecord::Base
+#   validates :username, :password, presence: true
+
+#   has_secure_password
+# end
+
+class Student
+  attr_reader :name
+  def initialize(args)
+    @name = args[:name]
+  end
 end
+
+class Organization
+  attr_accessor :members
+  def initialize(*student)
+    @members = []
+    @members << student
+    @members.flatten!
+  end
+
+  def add(*student)
+    members << student
+    members.flatten!
+  end
+
+  def remove(student)
+
+  end
+end
+
+ivan = Student.new(name: "Ivan Birkman")
+lucas = Student.new(name: "Lucas Santos")
+justin = Student.new(name: "Justin Gaba")
+rayan = Student.new(name: "Rayan Boutaleb")
+kevin = Student.new(name: "Kevin Alwell")
+
+squirrels = Organization.new(ivan, lucas)
+squirrels.add(justin)
+
+module Session
+  class << self
+    def random_pair(students)
+      students.sample(2)
+    end
+  end
+end
+
+p squirrels
