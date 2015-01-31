0f92f6083db3b27fabf8ae7e89726d81d4e7e91d
n-zeplo <nzeplowitz@gmail.com>
2015-01-31 10:44:46 -0500

    Clean up Test Code in Popup JS

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
   var pairing_duration_ms;
-  console.log("Loading...")
+
   $('button.duration-button').on('click', function(event){
     event.preventDefault();
-    console.log($('input.duration').val());
+
     if($('input.duration').val() !== "" ){
-      pairing_duration_ms = parseInt($('input.duration').val()) * 6000;
-      console.log(pairing_duration_ms)
+      pairing_duration_ms = parseInt( $('input.duration').val() ) * 6000;
     } else {
-      alert("Please Enter A Number")
+      alert("Please Enter A Number");
     }
   });
 
