494fa0c785d7a52af4f72a8c22ccbf1bec4590da
n-zeplo <nzeplowitz@gmail.com>
2015-01-31 17:56:24 -0500

    Add Comments

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,10 +1,8 @@
 post '/session' do
 	user1 = User.find_by(email: params["user1"]["email"])
+	# .try(:authenticate, params["user1"])
 	user2 = User.find_by(email: params["user2"]["email"])
-	p "*" * 40
-	p params["user2"]["email"]
-	p user1
-	p user2
+
 	if user1 && user2
 		Session.create(user1: user1, user2: user2)
 		{ :user1 => user1.email, :user2 => user2.email}.to_json
