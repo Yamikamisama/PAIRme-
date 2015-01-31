602563a83c181aaa81fd1ca394582ecceac1bf4f
Steph Reaves <stephanie.reaves@gmail.com>
2015-01-31 17:28:12 -0500

    basic session passing

---
+++ app/controllers/session.rb
@@ -0,0 +1,17 @@
+get '/session' do 
+
+	erb: :session
+end 
+
+post '/session' do
+	user1 = User.find_by(email: params[:user1][:email]).try(:authenticate, params[:user1]) 
+	user2 = User.find_by(email: params[:user2][:email]).try(:authenticate, params[:user2])  
+	
+	if user1 && user2 
+		Session.create(user1: user1, user2: user2)
+		json {user1: user1.email, user2: user2.email}
+	else 
+		500 
+	end 
+	
+end 
