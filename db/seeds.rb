b58f6c57ee505f5a6fa2be9d4da8ed992853c382
n-zeplo <nzeplowitz@gmail.com>
2015-02-03 15:25:10 -0500

    New Seed File For New Migrations/Associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,36 @@
-require 'faker'
+require "faker"
 
-organization = Organization.create(name: "DBC")
+nums = (1..5).to_a
+times = [20, 25, 30, 35, 40, 45, 60]
+organizations = ["DBC", "Owls", "Ospreys", "Google"]
 
-9.times do
-  Organization.create(name: Faker::Company.name)
+users = [
+{username: 'steph', email: 'steph@steph.com', password: '123456', driver_score: nums.sample, navigator_score: nums.sample},
+{username: 'me', email: 'me@me.com', password: '123456', driver_score: nums.sample, navigator_score: nums.sample},
+{username: 'test', email: 'test@test.com', password: '123456', driver_score: nums.sample, navigator_score: nums.sample},
+{username: 'nathan', email: 'nathan@nate.com.com', password: '123456', driver_score: nums.sample, navigator_score: nums.sample},
+{username: 'bob', email: 'bob@bob.com', password: '123456', driver_score: nums.sample, navigator_score: nums.sample},
+{username: 'jon', email: 'jon@doe.com', password: '123456', driver_score: nums.sample, navigator_score: nums.sample}
+]
+
+organizations.each { |org| Organization.create(name: org) }
+
+users.each {|user_info| Organization.all.sample.users << User.create(user_info)}
+
+10.times do
+  session = Session.create()
+  users = User.all.shuffle
+  user1 = users.first
+  user2 = users.last
+  session.users << user1
+  session.users << user2
+  Feedback.create(q1: nums.sample, q2: nums.sample, q3: nums.sample, content: Faker::Lorem.sentence, receiver: user1, giver: user2, session: session)
+  3.times do
+    Interval.create(active_time: times.sample, pause_time: times.sample, driver: user1, navigator: user2, session: session)
+    user = user1
+    user1 = user2
+    user2 = user
+  end
 end
-
-user1 = User.create(username: 'steph', email: 'steph@steph.com', password: '123456', driver_score: 5, navigator_score: 5, organization_id: 1)
-user2 = User.create(username: 'me', email: 'me@me.com', password: '123456', driver_score: 5, navigator_score: 5, organization_id: 3)
-
-9.times do
-  User.create(username: Faker::Internet.user_name, email: Faker::Internet.email, password: '123456', driver_score: 3, navigator_score: 4, organization_id: 3)
-end
-
-
-session = Session.create(duration: 60, pause_time: 30, work_time: 30, user1_drive_time: 15, user2_drive_time: 15, user1_id: user1, user2_id: user2);
 
 
