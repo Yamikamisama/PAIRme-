e781ac1f48213bf095d5091ef11116328c629c22
Steph Reaves <stephanie.reaves@gmail.com>
2015-02-13 20:45:02 -0500

    remove commented out code

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,22 +2,4 @@ class Feedback < ActiveRecord::Base
   belongs_to :giver, class_name: "User"
   belongs_to :receiver, class_name: "User"
   belongs_to :session
-
-
-  # def average(array)
-  # 	all_ratings = array
-  #   all_ratings.reduce(:+) / all_ratings.length
-  # end
-
-  # def driver_score
-  #   rating1
-  # end
-
-  # def navigator_score
-  #   rating2
-  # end
-
-  # def overall_score
-  #   rating3
-  # end
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -3,16 +3,4 @@ class Session < ActiveRecord::Base
   has_many :feedbacks
   has_many :user_sessions
   has_many :users, through: :user_sessions
-  # def add_feedback(feedback)
-  #   pair_id = [self.student1.id, self.student2.id]
-
-  #   if pair_id[0] == feedback.student.id
-  #     self.feedback1 = feedback
-  #     pair_id.delete(feedback.student.id)
-  #   end
-  #   if pair_id[1] == feedback.student.id
-  #     self.feedback2 = feedback
-  #     pair_id.delete(feedback.student.id)
-  #   end
-  # end
 end
