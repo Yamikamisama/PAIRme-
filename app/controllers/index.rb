77752e23cbf3ce4dd08042a6fb383bce0c7568b4
Michael Angelo <yamikamisama@gmail.com>
2015-03-13 17:47:57 -0400

    set profile page layout to false

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,5 +10,5 @@ get '/profile/:id' do |id|
 		@user = User.find(id)
 		@organizations = User.find(id).organizations
 		@feedbacks= Feedback.where(receiver_id: id)
-		erb :profile
+		erb :profile, layout: false
 end
